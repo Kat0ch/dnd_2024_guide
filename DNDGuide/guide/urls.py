18dd0c72d1210ac8b57334d1a00e988609d011ac
Kat0ch <130377438+Kat0ch@users.noreply.github.com>
2025-06-22 20:59:04 +0300

    Добавлена домашняя страница

---
+++ DNDGuide/guide/urls.py
@@ -0,0 +1,6 @@
+from .views import *
+from django.urls import path
+
+urlpatterns = [
+    path('', HomePage.as_view(), name='home')
+]
--- DNDGuide/guide/views.py
+++ DNDGuide/guide/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import *
+from django.views.generic import View
 
-# Create your views here.
+
+class HomePage(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'guide/home.html', {})
